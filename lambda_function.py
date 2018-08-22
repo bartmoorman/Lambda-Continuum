d44017975ea7a26f85180fa8ead7dd6776929176
Bart Moorman <github@id10t.us>
2018-08-21 23:43:10 -0600

    Return specific error on failure

--- lambda_function.py
+++ lambda_function.py
@@ -1,13 +1,23 @@
-import requests, json
+import requests
 
 def lambda_handler(event, context):
-  print(event)
-  timeout = event['timeout'] if 'timeout' in event else 5.0
+  timeout = event['timeout'] if 'timeout' in event else 1.0
   allow_redirects = event['allow_redirects'] if 'allow_redirects' in event else True
   verify = event['verify'] if 'verify' in event else True
   try:
     r = requests.get(event['url'], timeout=timeout, allow_redirects=allow_redirects, verify=verify)
-    return json.dumps({'total_seconds': r.elapsed.total_seconds(), 'status_code': r.status_code})
-  except requests.exceptions.RequestException as exception:
-    print(exception)
-    return False
+    return {'success': True, 'total_seconds': r.elapsed.total_seconds(), 'status_code': r.status_code, 'reason': r.reason}
+  except requests.exceptions.ReadTimeout:
+    return {'success': False, 'reason': 'ReadTimeout'}
+  except requests.exceptions.ConnectTimeout:
+    return {'success': False, 'reason': 'ConnectTimeout'}
+  except requests.exceptions.TooManyRedirects:
+    return {'success': False, 'reason': 'TooManyRedirects'}
+  except requests.exceptions.URLRequired:
+    return {'success': False, 'reason': 'URLRequired'}
+  except requests.exceptions.HTTPError:
+    return {'success': False, 'reason': 'HTTPError'}
+  except requests.exceptions.ConnectionError:
+    return {'success': False, 'reason': 'ConnectionError'}
+  except requests.exceptions.RequestException:
+    return {'success': False, 'reason': 'RequestException'}
