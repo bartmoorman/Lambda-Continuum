c466097859074df7f8dc1530f2c72108ba6f7f76
Bart Moorman <github@id10t.us>
2021-03-11 23:43:26 -0700

    Allow specifying HTTP method

--- lambda_function.py
+++ lambda_function.py
@@ -1,11 +1,12 @@
 import requests
 
 def lambda_handler(event, context):
+  method = event['method'] if 'method' in event else head
   timeout = event['timeout'] if 'timeout' in event else 1.0
   allow_redirects = event['allow_redirects'] if 'allow_redirects' in event else True
   verify = event['verify'] if 'verify' in event else True
   try:
-    r = requests.get(event['url'], timeout=timeout, allow_redirects=allow_redirects, verify=verify)
+    r = requests.request(method, event['url'], timeout=timeout, allow_redirects=allow_redirects, verify=verify)
     return {'success': True, 'total_seconds': r.elapsed.total_seconds(), 'status_code': r.status_code, 'reason': r.reason}
   except requests.exceptions.ReadTimeout:
     return {'success': False, 'reason': 'ReadTimeout'}
